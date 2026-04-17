6c00b2a0308c6fa478f84b7a8c2f5fbb801f20b2
yuvraj bahure <yuvrajbahure11@gmail.com>
2026-04-17 21:37:48 +0530

    User Authentication

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from forms import RegisterForm
+from models import User, db
+from flask import request
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+db.init_app(app)
 
 @app.route('/')
 def home():
@@ -9,6 +17,49 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(email=form.email.data).first()
+
+        if existing_user:
+            return "Email already registered. Please login."
+
+        new_user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return "User Registered Successfully"
+
+    return render_template("register.html", form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+
+    if request.method == 'POST':
+
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.password == password:
+            return "Login Successful"
+
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    return "User Logged Out"
 
 if __name__ == '__main__':
     app.run(debug=True)
